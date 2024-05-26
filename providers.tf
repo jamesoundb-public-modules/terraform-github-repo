661618d9c51f5888205c1769ef2a1d33fb730ec5
James Wurbel <jameswurbel@gmail.com>
2024-05-26 15:43:34 -0700

    Fixing credentials

---
+++ providers.tf
@@ -0,0 +1,17 @@
+terraform {
+  required_providers {
+    github = {
+      source  = "integrations/github"
+      version = "5.16.0"
+      # Set GITHUB_TOKEN environment variable or include token reference
+    }
+    tfe = {
+      source  = "hashicorp/tfe"
+      version = "0.41.0"
+    }
+  }
+}
+
+provider "github" {
+  token = var.github_token
+}
